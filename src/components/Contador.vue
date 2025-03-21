9c287b572da5e86e5f98bad89740d88f1862f577
Belen <lobato.alex12@gmail.com>
2025-03-21 13:39:53 -0600

    Crea componete de contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,24 @@
+<template>
+<h1>hola mundo</h1>
+<p>{{ contador }} </p>
+<button @click="Incrementar">Incrementar</button>
+<button @click="Decrementar">Decrementar</button>
+</template>
+
+<!-- se recomienda poner el script setup  debajo del template-->
+<script setup>
+ import {ref} from 'vue';
+ const contador = ref(0);
+ const Incrementar = () =>{
+   contador.value++;
+ }
+ const Decrementar = () =>{
+  contador.value --;
+ }
+</script>
+
+<style scoped>
+p{
+  color: red;
+}
+</style>
